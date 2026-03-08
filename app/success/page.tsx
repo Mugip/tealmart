25969535a46a8f399806435c3e4a44dcf370c33e
Mugip <141583223+Mugip@users.noreply.github.com>
2026-03-08 11:02:52 +0300

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,8 @@
-'use client'
+// app/success/page.tsx
+export const dynamic = 'force-dynamic'
 
 import { Suspense } from 'react'
 import SuccessContent from './SuccessContent'
-
-export const dynamic = 'force-dynamic'
 
 export default function SuccessPage() {
   return (
