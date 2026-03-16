5b0d5657492dc00129a3656bc10bd1d9e10f3a8f
Mugisha Pius <141583223+Mugip@users.noreply.github.com>
2026-03-16 13:03:45 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,6 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    screens: {
-      'xs': '375px',   // Add extra-small breakpoint
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
     extend: {
       colors: {
         tiffany: {
