0603ac4f1f2bf4471f126eb511d255d73d30510d
Mugisha Pius <141583223+Mugip@users.noreply.github.com>
2026-03-16 12:54:09 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'xs': '375px',   // Add extra-small breakpoint
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       colors: {
         tiffany: {
@@ -14,7 +22,7 @@ module.exports = {
           200: '#99f6e4',
           300: '#5eead4',
           400: '#2dd4bf',
-          500: '#14b8a6', // Tiffany blue primary
+          500: '#14b8a6',
           600: '#0d9488',
           700: '#0f766e',
           800: '#115e59',
@@ -24,7 +32,7 @@ module.exports = {
           50: '#fdfcfb',
           100: '#faf8f5',
           200: '#f5f1ea',
-          300: '#ede7dc', // Katak (milky tea) primary
+          300: '#ede7dc',
           400: '#e3d9c8',
           500: '#d4c5ad',
           600: '#bfac8f',
@@ -32,6 +40,22 @@ module.exports = {
           800: '#86755c',
           900: '#6f614c',
         },
+      },
+      spacing: {
+        '18': '4.5rem',
+        '88': '22rem',
+        '128': '32rem',
+      },
+      fontSize: {
+        'xs': ['0.75rem', { lineHeight: '1.5' }],
+        'sm': ['0.875rem', { lineHeight: '1.5' }],
+        'base': ['1rem', { lineHeight: '1.6' }],
+        'lg': ['1.125rem', { lineHeight: '1.6' }],
+        'xl': ['1.25rem', { lineHeight: '1.6' }],
+        '2xl': ['1.5rem', { lineHeight: '1.5' }],
+        '3xl': ['1.875rem', { lineHeight: '1.4' }],
+        '4xl': ['2.25rem', { lineHeight: '1.3' }],
+        '5xl': ['3rem', { lineHeight: '1.2' }],
       },
     },
   },
