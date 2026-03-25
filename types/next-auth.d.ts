b322b63c2b60ed0c623660e06bd673439ee673d8
Mugisha Pius <141583223+Mugip@users.noreply.github.com>
2026-03-25 09:30:20 +0300

    Update next-auth.d.ts

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,16 +1,26 @@
-import NextAuth from "next-auth"
-import { DefaultSession } from "next-auth"
+// types/next-auth.d.ts
+import NextAuth from 'next-auth'
 
-declare module "next-auth" {
+declare module 'next-auth' {
   interface Session {
     user: {
       id: string
-    } & DefaultSession["user"]
+      name?: string | null
+      email?: string | null
+      image?: string | null
+      isAdmin?: boolean
+    }
+  }
+
+  interface User {
+    id: string
+    isAdmin?: boolean
   }
 }
 
-declare module "next-auth/jwt" {
+declare module 'next-auth/jwt' {
   interface JWT {
     id: string
+    isAdmin?: boolean
   }
 }
