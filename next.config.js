b0489c794d38951e64ff04fb07a65299175ddf93
Mugisha Pius <141583223+Mugip@users.noreply.github.com>
2026-03-28 13:10:57 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,20 +2,27 @@
 const nextConfig = {
   images: {
     remotePatterns:[
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      }
+      { protocol: 'https', hostname: '**' },
+      { protocol: 'http', hostname: '**' }
     ],
   },
-  // Suppress warnings for draft features if any
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return[
+      {
+        // Reroute browsers looking for an old-school .ico file to your modern SVG
+        source: '/favicon.ico',
+        destination: '/logo.svg',
+      },
+      {
+        // Reroute Facebook/Twitter looking for an OpenGraph image to your SVG
+        source: '/og-image.png',
+        destination: '/logo.svg', 
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
